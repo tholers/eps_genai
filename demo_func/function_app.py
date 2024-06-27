a1d3bf1404dcce5abbded9c93900325e2c8df77d
Le Tho (Lê Thọ) <tho.le@rackspace.com>
2024-06-27 13:42:43 +0700

    fix bug in the recursive logic, add folder structure, add date filter

--- demo_func/function_app.py
+++ demo_func/function_app.py
@@ -17,29 +17,18 @@ import logging
 app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 
 
-def get_list_subfolder(ctx,rootFolder):
-    '''
-    ctx: context
-    '''
-    libraryRoot = ctx.web.get_folder_by_server_relative_url(rootFolder)
-    Subfolders = libraryRoot.folders
-    ctx.load(Subfolders)
-    ctx.execute_query()
-    sub_folder = [i.properties['ServerRelativeUrl'] for i in Subfolders]
-
-    return sub_folder
-
 def get_list_subfolder_v2(ctx,rootFolder, recursive=False):
     '''
     ctx: context
     '''
-    # final_subfolder = list()
-    final_subfolder = []
+    final_subfolder = [] # store all subfolders recursively
 
+    # get the subfolders of rootFolder only
     subfolder = ctx.web.get_folder_by_server_relative_url(rootFolder).folders
     ctx.load(subfolder)
     ctx.execute_query()
     
+    # for each subfolder above, recursively add all its subfolders
     if recursive == True:
         for folder in subfolder:
             final_subfolder.extend(
@@ -51,34 +40,9 @@ def get_list_subfolder_v2(ctx,rootFolder, recursive=False):
             )
 
     final_subfolder.extend([i.properties['ServerRelativeUrl'] for i in subfolder])
-    #final_subfolder = [i.properties['ServerRelativeUrl'] for i in subfolder]
 
     return final_subfolder
 
-
-def get_list_file(ctx,rootFolder,last_extract_date,re_file=None):
-    '''
-    ctx: context
-    last_extract_date: 
-        -only get files from this date onwards (e.g. useful for incremental loading)
-        -must be of the form '2024-04-23T17:00:00.000+00:00', so that we can .split('T')
-    re_file: what is it? in file.properties['Name']
-    '''
-    libraryRoot = ctx.web.get_folder_by_server_relative_url(rootFolder)
-    files = libraryRoot.files
-    # files_modified = libraryRoot.time_last_modified
-    ctx.load(files)
-    ctx.execute_query()
-    list_file = []
-    for file in files:
-        if re_file == None and dt.datetime.strftime(file.properties['TimeLastModified'].date(), "%Y-%m-%d") >= last_extract_date.split('T')[0]:
-            list_file.append(file.properties['ServerRelativeUrl']) #get path of file is 'ServerRelativeUrl'
-
-        # need to define what happen if we are not reading latest files like above
-        elif re_file.lower() in file.properties['Name'].lower() and file.properties['TimeLastModified'].split('T')[0] >= last_extract_date.split('T')[0]:#get data be modified from now date - 1
-            list_file.append(file.properties['ServerRelativeUrl']) #get path of file is 'ServerRelativeUrl'
-           
-    return list_file
 
 def get_list_file_v2(ctx,rootFolder,last_extract_date,re_file=None, recursive=False):
     '''
@@ -88,11 +52,14 @@ def get_list_file_v2(ctx,rootFolder,last_extract_date,re_file=None, recursive=Fa
         -must be of the form '2024-04-23T17:00:00.000+00:00', so that we can .split('T')
     re_file: what is it? in file.properties['Name']
     '''
-    final_file = []
+    final_file = []# store all files recursively
     files = ctx.web.get_folder_by_server_relative_url(rootFolder).files
     ctx.load(files)
     ctx.execute_query()
-    final_file.extend([file.properties['ServerRelativeUrl'] for file in files])
+
+    final_file.extend([file.properties['ServerRelativeUrl'] for file in files if
+                       dt.datetime.strftime(file.properties['TimeLastModified'].date(), "%Y-%m-%d") 
+                       >= last_extract_date.split('T')[0] ])
     
     
     final_subfolder = []
@@ -105,8 +72,10 @@ def get_list_file_v2(ctx,rootFolder,last_extract_date,re_file=None, recursive=Fa
         final_subfolder = get_list_subfolder_v2(
                     ctx = ctx,
                     rootFolder = rootFolder,
-                    recursive = recursive
-        )
+                    recursive = False
+        ) # recusive should be false here, since the recusive property is already
+        # taken care by the following get list file logic already.
+        # if we use recursive=True here, it will keep repeating subfolders
 
         for folder in final_subfolder:
             final_file.extend(
@@ -127,6 +96,33 @@ def get_list_file_v2(ctx,rootFolder,last_extract_date,re_file=None, recursive=Fa
     return final_file
 
 
+def download_file(ctx,rootFolder,connection_string,container_name,service, recursive=False):
+    '''
+    '''
+    final_file = []# store all files recursively
+    files = ctx.web.get_folder_by_server_relative_url(rootFolder).files
+    ctx.load(files)
+    ctx.execute_query()
+    final_file.extend([file.properties['ServerRelativeUrl'] for file in files])
+    
+    blob_folder = "test2"
+    for file_url in final_file:
+        bytes_file_obj = io.BytesIO()
+        ctx.web.get_file_by_server_relative_path(file_url).download(bytes_file_obj).execute_query()
+        file_name = file_url.split('/')[-1]
+        # blob_folder_batch = file_url.partition("VOYAGE FILE")[-1]
+        # first, get rid of everything before and including "VOYAGE FILE"
+        # then extract only the dir of the remaining (i.e. get rid of file part)
+        blob_folder_batch = os.path.dirname(file_url.partition("VOYAGE FILE")[-1])
+
+        # where to put the file in blob folder
+        # path_new_file = f'{blob_folder}/{file_name}'   
+        path_new_file = f'{blob_folder}/{blob_folder_batch}/{file_name}' 
+        blob_client = service.get_blob_client(container=container_name,blob = path_new_file)
+        blob_client.upload_blob(bytes_file_obj.getvalue(),overwrite=True)
+
+    return final_file
+
 
 @app.route(route="HttpExample")
 def HttpExample(req: func.HttpRequest) -> func.HttpResponse:
@@ -137,7 +133,9 @@ def HttpExample(req: func.HttpRequest) -> func.HttpResponse:
     client_secret   = os.environ["secret_value"]
 
     # Configuration to connect blob storage
-    # req_body = req.get_json() # need to send a json, o/w error
+    req_body = req.get_json() # need to send a json, o/w error
+    print(req_body.get("a"))
+    print(req_body.get("b"))
 
     connection_string = os.environ["storage_account"]
     container_name = os.environ["blob_container"]              
@@ -174,32 +172,54 @@ def HttpExample(req: func.HttpRequest) -> func.HttpResponse:
     # )
 
     # -------Testing with files
-    # sharepoint_folder = "EPS Filing System/VOYAGE FILE/EPS23652/BBB/9 - Miscellaneous Communication"
+    sharepoint_folder = "EPS Filing System/VOYAGE FILE/EPS23652/BBB/9 - Miscellaneous Communication"
     # sharepoint_folder = "EPS Filing System/VOYAGE FILE/ADRIATIC SEA/1004/20/1 - Voyage Fixture"
-    sharepoint_folder = "EPS Filing System/VOYAGE FILE/ADRIATIC SEA/1004/20/"
+    # sharepoint_folder = "EPS Filing System/VOYAGE FILE/ADRIATIC SEA/1004/20/"
+    # sharepoint_folder = "EPS Filing System/VOYAGE FILE/ADRIATIC SEA/1006"
+    # sharepoint_folder = "EPS Filing System/VOYAGE FILE/ADRIATIC SEA/1004/"
+    # sharepoint_folder = "EPS Filing System/VOYAGE FILE/ADRIATIC SEA/"
+    # sharepoint_folder = "EPS Filing System/VOYAGE FILE/ATLANTIC EMERALD"
+    # sharepoint_folder = "EPS Filing System/VOYAGE FILE/"
     last_extract_date = '2020-01-01T00:00:00'
     # last_extract_date = '2024-06-21T00:00:00'
+    # last_extract_date = '2024-01-01T00:00:00'
+    # last_extract_date = '2024-07-01T00:00:00'
     re_file = None
+    
     list_file = get_list_file_v2(ctx,
                               rootFolder = sharepoint_folder,
                               last_extract_date = last_extract_date,
                               re_file = re_file,
                               recursive=True)
     
-    # print(len(list_file))
-    # list_file = ['123']
-
-    blob_folder = "test"
+    print('done getting list file, now working on downloading file')
+    blob_folder = "test2"
     for file_url in list_file:
+        # print(file_url)
+        # print(1)
         bytes_file_obj = io.BytesIO()
+
         ctx.web.get_file_by_server_relative_path(file_url).download(bytes_file_obj).execute_query()
         file_name = file_url.split('/')[-1]
+        blob_folder_batch = os.path.dirname(file_url.partition("VOYAGE FILE")[-1])[1:]
 
         # where to put the file in blob folder
-        path_new_file = f'{blob_folder}/{file_name}'    
+        # path_new_file = f'{blob_folder}/{file_name}' 
+        path_new_file = f'{blob_folder}/{blob_folder_batch}/{file_name}'  
+        # print(path_new_file)
         blob_client = service.get_blob_client(container=container_name,blob = path_new_file)
+        # print(2)
         blob_client.upload_blob(bytes_file_obj.getvalue(),overwrite=True)
+        # print(3)
 
+    print('done uploading all files')
+
+    # list_file = download_file(ctx,
+    #                         rootFolder = sharepoint_folder,
+    #                         connection_string=connection_string,
+    #                         container_name=container_name,
+    #                         service=service,
+    #                         recursive=False)
 
     return func.HttpResponse(
         f"This HTTP triggered function executed successfully.\n {len(list_file), list_file}",
